3b6caa9ae872fbda65613ac930d122216bd7ec7c
Sam Gee <samgee598@gmail.com>
2025-05-22 13:54:19 -0400

    Form and functionality added

--- js/index.js
+++ js/index.js
@@ -43,3 +43,53 @@ for (let i = 0; i < skills.length; i++) {
     skillsList.appendChild(skill);
     console.log(skillsList);
 }
+
+//remove button callback function
+function clickRemove(event) {
+    const entry = event.target.parentNode;
+    entry.remove();
+}
+
+//form submit callback function
+function formSubmit(event) {
+    event.preventDefault();
+
+    //retrieving message section data
+    const formData = new FormData(event.target);
+    const name = formData.get("usersName");
+    const email = formData.get("usersEmail");
+    const message = formData.get("usersMessage");
+    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+
+    //find message section
+    const messageSection = document.getElementById("Messages");
+    const messageList = messageSection.getElementsByTagName("ul");
+    console.log(messageList);
+
+    //display message section
+    const newMessage = document.createElement("li");
+    const messageDisplay = `<a href="mailto:${email}">${name}</a>\n
+    <span>${message}</span>`;
+    newMessage.innerHTML = messageDisplay;
+
+    //adding remove button
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.setAttribute("type", "button");
+    //remove button event listener function
+    removeButton.addEventListener("click", clickRemove);
+
+    //appending remove button to new message
+    newMessage.appendChild(removeButton);
+
+    //appending new message to message list
+    messageList[0].appendChild(newMessage);
+
+    event.target.reset();
+}
+
+//finding leave message form
+const messageForm = document.getElementsByName("leave_message");
+console.log(messageForm);
+//message form event listener function
+messageForm[0].addEventListener("submit", formSubmit);
